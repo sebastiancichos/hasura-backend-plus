da83cc0fb00ae84850c83d0e04a71fb3c4b2dc62
Sebastian Cichos <sebastiancichos@outlook.com>
2020-03-10 05:11:35 +0100

    test

--- src/mailer/mailer.js
+++ src/mailer/mailer.js
@@ -4,7 +4,7 @@ let router = express.Router();
 
 router.post("/", function(req, res) {
   console.log(req.body);
-  res.json({ status: "ok", mailer: req.data });
+  res.json({ status: "ok", mailer: req.body });
 });
 
 module.exports = router;
